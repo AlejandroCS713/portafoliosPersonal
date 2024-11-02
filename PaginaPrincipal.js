c2b6415b0a5e27d514d91c0288ef92a7085cc10e
AlejandroCS713 <alejandro2003carbonell@gmail.com>
2024-11-02 12:40:49 +0100

    Trabajando en el contacto

--- PaginaPrincipal.js
+++ PaginaPrincipal.js
@@ -13,24 +13,25 @@ window.addEventListener("scroll", function() {
     lastScrollTop = scrollTop;
 });
 
-// Ocultar todas las secciones menos la seleccionada
+// Función para mostrar solo la sección seleccionada
 function showSection(sectionId) {
-    const sections = document.querySelectorAll("main section");
+    const sections = document.querySelectorAll("main section"); // Selecciona todas las secciones en el main
     sections.forEach(section => {
-        section.classList.remove("active");
         if (section.id === sectionId) {
-            section.classList.add("active");
+            section.classList.add("active"); // Añade la clase 'active' a la sección actual
+        } else {
+            section.classList.remove("active"); // Remueve 'active' de otras secciones
         }
     });
 }
 
 // Agregar eventos de clic a los enlaces de navegación
-const navLinks = document.querySelectorAll("nav ul li a");
+const navLinks = document.querySelectorAll("nav ul li a"); // Selecciona todos los enlaces en el nav
 navLinks.forEach(link => {
     link.addEventListener("click", function(event) {
-        event.preventDefault(); // Evitar el desplazamiento
-        const targetSection = link.getAttribute("data-target");
-        showSection(targetSection);
+        event.preventDefault(); // Evita que el enlace se comporte de forma predeterminada
+        const targetSection = link.getAttribute("data-target"); // Obtiene el valor de data-target
+        showSection(targetSection); // Muestra la sección deseada
     });
 });
 
@@ -38,3 +39,25 @@ navLinks.forEach(link => {
 document.addEventListener("DOMContentLoaded", () => {
     showSection("sobre-mi"); // Muestra "Sobre Mí" como sección inicial
 });
+
+// URL del perfil de GitHub
+const githubUsername = 'AlejandroCS713';
+const followersCountElement = document.getElementById('github-followers');
+
+// Función para obtener el número de seguidores de GitHub
+async function getGitHubFollowers() {
+    try {
+        const response = await fetch(`https://api.github.com/users/${githubUsername}`);
+        const data = await response.json();
+        if (data && data.followers !== undefined) {
+            followersCountElement.textContent = data.followers;
+        } else {
+            console.error('No se pudo obtener el número de seguidores.');
+        }
+    } catch (error) {
+        console.error('Error al obtener el número de seguidores:', error);
+    }
+}
+
+// Llamar a la función cuando se carga la página
+window.onload = getGitHubFollowers;
